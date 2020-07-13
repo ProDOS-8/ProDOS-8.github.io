3f6cb625b5ce0979e6488827ae9636a57e123917
Devon Hubner <devon@hubner.org>
2020-07-12 22:12:04 -0400

    Updated 2.5a8 on the index

--- update_2.5.sh
+++ update_2.5.sh
@@ -1,4 +1,7 @@
-st _includes/topbar.html _pages/releases/prodos-25.md _data/navigation.yml
+st _includes/topbar.html \
+   _pages/releases/prodos-25.md \
+   _data/navigation.yml \
+   _layouts/home.html
 
 read -p "When finished with update, press RETURN to run jekyll build and s3_website push"
 
